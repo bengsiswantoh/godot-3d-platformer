1f519e6f19d93d0c1cb4bc129da153332aad4b4b
bengsiswanto <bengsiswantoh@gmail.com>
2024-05-23 22:56:46 +0700

    change environment

--- scripts/characters/enemy/enemy_return_state.gd
+++ scripts/characters/enemy/enemy_return_state.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	
 	
 func _physics_process(_delta: float) -> void:
-	if (character.agent.is_navigation_finished()):
+	if character.agent.is_navigation_finished():
 		character.state_machine.switch_state(EnemyPatrolState)
 		return
 	
